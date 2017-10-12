4784c80f78eab7e0d6235920e8a5a8859931bb8e
Danka Trtovic <danka@webcentric.co.rs>
2017-10-12 15:18:42 +0200

    Prvi

---
+++ src/containers/serch_bar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component{
+    constructor(props){
+        super(props);
+        this.state = {term : ''};
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+    onInputChange(e){
+        this.setState({term : e.target.value});
+    }
+    onFormSubmit(e){
+        e.preventDefault();
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.onFormSubmit} className="input-group">
+                <input onChange={this.onInputChange} value={this.state.term} placeholder="pera" className="form-control" />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary"> Submit</button>
+                </span>
+            </form>
+        );
+    }
+}
