232eca22f525a33d0748bdcd84858f2a58a319b3
Danka Trtovic <danka@webcentric.co.rs>
2017-10-13 10:38:49 +0200

    API kljuc popravljen

--- src/containers/serch_bar.js
+++ src/containers/serch_bar.js
@@ -3,25 +3,27 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component{
+class SearchBar extends Component{
     constructor(props){
         super(props);
         this.state = {term : ''};
 
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
     onInputChange(e){
         this.setState({term : e.target.value});
     }
     onFormSubmit(e){
         e.preventDefault();
-
+        this.props.fetchWeather(this.state.term);
+        this.setState({term: ''});
     }
 
     render(){
         return(
             <form onSubmit={this.onFormSubmit} className="input-group">
-                <input onChange={this.onInputChange} value={this.state.term} placeholder="pera" className="form-control" />
+                <input onChange={this.onInputChange} value={this.state.term} placeholder="Trazi" className="form-control" />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary"> Submit</button>
                 </span>
@@ -29,3 +31,7 @@ export default class SearchBar extends Component{
         );
     }
 }
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+export default connect(null, mapDispatchToProps)(SearchBar);
